463b1c10960165af94cbf35796c36dcd1a3a132a
yosiaking <yoshiaki.im@gmail.com>
2016-04-28 15:26:14 +0900

    Initial Commit

---
+++ app/src/main/java/com/example/sample/dungeonrpg/MainActivity.java
@@ -0,0 +1,249 @@
+package com.example.sample.dungeonrpg;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import Gameplay.Explore.GoDungeon001;
+import Object.Character.CreatureType.Human;
+import Object.Character.Player.Hero;
+import Object.Dungeon.DungeonBase;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //スクロールビュー
+    private ScrollView scrollView;
+
+    //メッセージarray
+    private ArrayList<TextView> messageList;
+
+    //レイアウト変数
+    private LinearLayout storyTellingLog;
+
+    //ダンジョン生成
+    private GoDungeon001 dungeon;
+    private DungeonBase dungeon001;
+
+    //ヒーロー生成
+    private Hero h;
+
+    //ボタン変数
+    private Button goButton;
+    private Button attackButton;
+    private Button magicButton;
+    private Button recoverButton;
+    private Button statusButton;
+    private Button escapeButton;
+
+    //メニュー変数
+    private TextView menuName;
+    private TextView menuHp;
+    private TextView menuMp;
+    private TextView menuJewel;
+    private TextView menuFloor;
+    private TextView menuDistance;
+
+
+    //Context変数
+    Context context;
+
+    //レイアウトパラメータ
+    private final ViewGroup.LayoutParams WCWC = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+    private final ViewGroup.LayoutParams MPMP = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+    private final ViewGroup.LayoutParams WCMP = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+    private final ViewGroup.LayoutParams MPWC = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //物語表示板
+        storyTellingLog = (LinearLayout) findViewById(R.id.storyTellingLog);
+
+        //Context取得
+        context = getApplicationContext();
+
+        //ボタン
+        goButton = (Button) findViewById(R.id.go);
+        attackButton = (Button) findViewById(R.id.attack);
+        magicButton = (Button) findViewById(R.id.magic);
+        recoverButton = (Button) findViewById(R.id.recover);
+        statusButton = (Button) findViewById(R.id.status);
+        escapeButton = (Button) findViewById(R.id.escape);
+
+        //クリックリスナー
+        goButton.setOnClickListener(this);
+        attackButton.setOnClickListener(this);
+        magicButton.setOnClickListener(this);
+        recoverButton.setOnClickListener(this);
+        statusButton.setOnClickListener(this);
+        escapeButton.setOnClickListener(this);
+
+        //メニューカウント取得
+        menuName = (TextView) findViewById(R.id.heroName);
+        menuHp = (TextView) findViewById(R.id.heroHp);
+        menuMp = (TextView) findViewById(R.id.heroMp);
+        menuJewel = (TextView) findViewById(R.id.jewelCount);
+        menuFloor = (TextView) findViewById(R.id.floorCount);
+        menuDistance = (TextView) findViewById(R.id.distanceCount);
+
+        //音声
+
+
+        //ボタンの初期設定
+        exploreButtonSet();
+
+        //主人公の生成
+        h = new Hero("ヨシアキ", 100, 100, 50, 50);
+
+        //メニューカウント更新
+        menuCountSet();
+
+//        //ダンジョン生成
+        dungeon = new GoDungeon001();
+        dungeon001 = dungeon.dungeonGenerate(h, 1, context);
+    }
+
+
+
+    //戦闘中ボタンセット
+    private void battleButtonSet(){
+        goButton.setEnabled(false);
+        attackButton.setEnabled(true);
+        magicButton.setEnabled(true);
+        recoverButton.setEnabled(true);
+        statusButton.setEnabled(true);
+        escapeButton.setEnabled(false);
+    }
+
+    //探索中ボタンセット
+    private void exploreButtonSet(){
+        goButton.setEnabled(true);
+        attackButton.setEnabled(false);
+        magicButton.setEnabled(false);
+        recoverButton.setEnabled(true);
+        statusButton.setEnabled(true);
+        escapeButton.setEnabled(true);
+    }
+
+    //ボタンすべて非表示
+    private void allButtonNoSet(){
+        goButton.setEnabled(false);
+        attackButton.setEnabled(false);
+        magicButton.setEnabled(false);
+        recoverButton.setEnabled(false);
+        statusButton.setEnabled(false);
+        escapeButton.setEnabled(false);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+            case R.id.go :
+
+                //ストーリーメッセージ インスタンス化
+                messageList = new ArrayList<TextView>();
+                TextView text = new TextView(this);
+                text.setText("【すすむ】");
+
+                messageList.add(text);
+
+
+
+                TextView enemy = new TextView(this);
+                enemy.setText("敵の攻撃");
+                messageList.add(enemy);
+
+//                TextView hero = new TextView(this);
+//                hero.setText("味方ですよ");
+
+                //進行
+                messageList.addAll(dungeon001.goForward());
+
+                for(int i=0; i < messageList.size(); i++){
+
+                    //damageタグを持つTextViewは赤で表示
+                    if(messageList.get(i).getTag() == "damage"){
+                        messageList.get(i).setTextColor(Color.parseColor("#ff6666"));
+                    }
+                    //recoverタグを持つTextViewは青で表示
+                    if(messageList.get(i).getTag() == "recover"){
+                        messageList.get(i).setTextColor(Color.parseColor("#6666ff"));
+                    }
+
+                    storyTellingLog.addView(messageList.get(i));
+                }
+
+
+
+
+//                //物語表示TextViewで動的表示
+//                storyTellingText = new TextView(getApplicationContext());
+//                storyTellingText.setTextColor(Color.parseColor("#3399ff"));
+//                storyTellingText.setText(text);
+
+
+
+                break;
+
+            case R.id.recover :
+
+                break;
+
+            case R.id.status :
+
+                break;
+
+            case R.id.attack :
+
+                break;
+
+            case R.id.magic :
+
+                break;
+
+            case R.id.escape :
+
+                break;
+        }
+
+        //メニューカウント更新
+        menuCountSet();
+
+        //スクロールビューを最下部に移動
+        scrollView= (ScrollView)findViewById(R.id.storyScroll);
+        scrollView.post(new Runnable()
+        {
+            public void run()
+            {
+                scrollView.fullScroll(ScrollView.FOCUS_DOWN);
+            }
+        });
+
+    }
+
+
+    private void menuCountSet(){
+
+        menuName.setText("" + h.getName());
+        menuHp.setText("" + h.getHp() + "/" + h.getMaxHp());
+        menuMp.setText("" + h.getMp() + "/" + h.getMaxMp());
+        menuJewel.setText("" + Human.getCountJewel());
+        menuFloor.setText("" + Human.getDungeonFloor());
+        menuDistance.setText("" + Human.getDistance());
+
+    }
+
+}
